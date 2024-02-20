5f6e40a82ad2f7d4269d91011e6e7344d7254ebb
cpaguia <cpaguia@hawk.iit.edu>
2024-02-19 19:21:13 -0600

    working on layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,79 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const Placeholder());
+  UserInfo userInfo = UserInfo(
+    name: 'Shayan Baig',
+    about: 'Software Engineer',
+    uid: '@shayanb',
+    interests: ['coding', 'rock music', 'cats'],
+    school: 'IIT',
+    pronouns: ['he', 'him'],
+    city: 'Chicago, IL',
+  );
+
+  runApp(MaterialApp(home: UserInfoPage(userInfo: userInfo), title: 'Profile of @shayanb'));
+} 
+
+class UserInfo {
+  final String name;
+  final String about;
+  final String uid;
+  final List<String> interests;
+  final String school;
+  final List<String> pronouns;
+  final String city;
+
+  UserInfo({
+    required this.name,
+    required this.about,
+    required this.uid,
+    required this.interests,
+    required this.school,
+    required this.pronouns,
+    required this.city
+  });
 }
+
+class UserInfoPage extends StatelessWidget {
+  final UserInfo userInfo;
+
+  const UserInfoPage({super.key, required this.userInfo});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(userInfo.name),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Row(
+                children: [
+                  Image.asset('images/shayan.jpg')
+                ],
+              ),
+              const Row(
+                children: [
+                  Placeholder()
+                ]
+              )
+            ],
+          )
+        ],
+      )
+      
+      // const Center(
+      //   child: DecoratedBox(
+      //     decoration: BoxDecoration(color: Colors.lightGreenAccent),
+      //     child: Padding(
+      //       padding: EdgeInsets.all(8.0),
+      //       child: Text('Shayan'),
+      //     )
+      //   ),
+      // ),
+    );
+  }
+}
+
